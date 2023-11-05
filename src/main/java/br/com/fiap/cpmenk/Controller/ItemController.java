c8034a945268456948c875f1118ee453eaa08e14
brunoecp <rm95315@fiap.com.br>
2023-11-05 18:00:45 -0300

    change

--- src/main/java/br/com/fiap/cpmenk/Controller/ItemController.java
+++ src/main/java/br/com/fiap/cpmenk/Controller/ItemController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.cpmenk.models.Item;
-import br.com.fiap.cpmenk.models.Viagem;
 import br.com.fiap.cpmenk.repository.ItemRepository;
 import br.com.fiap.cpmenk.repository.ViagemRepository;
 
--- src/main/java/br/com/fiap/cpmenk/Controller/ViagemController.java
+++ src/main/java/br/com/fiap/cpmenk/Controller/ViagemController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.fiap.cpmenk.models.Item;
+
 import br.com.fiap.cpmenk.models.Viagem;
 import br.com.fiap.cpmenk.repository.UsuarioRepository;
 import br.com.fiap.cpmenk.repository.ViagemRepository;
